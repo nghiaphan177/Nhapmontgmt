d34bdb00dc565bd80b7ed1cba9bfc4709cbe7dad
phan-le-phu <62412437+phan-le-phu@users.noreply.github.com>
2020-12-25 09:06:32 +0700

    Add files via upload

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import cv2 as cv
 from matplotlib import pyplot as plt
 import pickle
 import os
+import time
 
 
 def load_model(file='model.pickle'):
@@ -13,13 +14,30 @@ def load_model(file='model.pickle'):
 
 
 def remove_green(img, model):
-    shape = img.shape
-    pixcels = img.copy().reshape(-1, 3)
-    for i in range(pixcels.shape[0]):
-        features = np.append(pixcels[i].reshape(1, -1), [1]).reshape(1, -1)
-        if model.predict(features)[0] == str(1):
-            pixcels[i] = np.array([255, 255, 255])
-    newImg = pixcels.reshape(shape)
+
+    newImg = img.copy()
+
+    w3, w2, w1, w0 = model.coef_[0]
+
+    R = img[:, :, 0]
+    G = img[:, :, 1]
+    B = img[:, :, 2]
+    inter = np.ones(R.shape)
+
+    mask = w3 * R + w2 * G + w1 * B + w0 * inter
+
+    mask = 1 / (1 + 1/np.exp(mask))
+
+    newImg[mask > 0.99999999] = [0, 0, 0]
+
+
+    # shape = img.shape
+    # pixcels = img.copy().reshape(-1, 3)
+    # for i in range(pixcels.shape[0]):
+    #     features = np.append(pixcels[i].reshape(1, -1), [1]).reshape(1, -1)
+    #     if model.predict(features)[0] == str(1):
+    #         pixcels[i] = np.array([255, 255, 255])
+    # newImg = pixcels.reshape(shape)
     return newImg
 
 
@@ -29,7 +47,10 @@ if __name__ == '__main__':
     pathImg = os.path.join(path, 'test_data', nameImg)
     img = cv.imread(pathImg, 1)
 
+    start = time.time()
     model = load_model()
+    print(time.time() - start)
+
     newImg = remove_green(img, model)
     plt.subplot(1, 2, 1);
     plt.imshow(img)
